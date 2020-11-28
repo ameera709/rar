fe51554abdce448c1022b4128bb691f5cb0c8dd2
RUBA040 <74141278+RUBA040@users.noreply.github.com>
2020-11-29 00:33:16 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,18 +1,12 @@
 <!DOCTYPE HTML>
-<html lan="ar" >
+<html >
 <head>
-
-
 <!-- jQuery (necessary JavaScript plugins) -->
-
 <!-- Custom Theme files -->
 <link href="css/style.css" rel='stylesheet' type='text/css' />
 <link rel="stylesheet" href="css/bootstrap-rtl.min.css"/>
 <!-- Custom Theme files -->
 <!--//theme-style-->
-
-
-
 
 </head>
 <body  >
@@ -31,8 +25,8 @@
 		</div>
 		<div class="clearfix"> </div>
 
-			 <h2 style='text-align:center;color:white;'>!CONCAT US</h2>
-			 <h3 style='text-align:center;'><a href="mailto:rar.covid19@gmail.com">rar.covid19@gmail.com </a></h3>
+			 <h2 style='text-align:center; color:white;'>!CONCAT US</h2>
+			 <h3 style='text-align:center;'><a style="color:gray; font-size:small;" href="mailto:rar.covid19@gmail.com">rar.covid19@gmail.com </a></h3>
 	         <p  style='text-align:center;color: gold; font-size:40px;'>____</p>
 
 
@@ -41,8 +35,9 @@
 </div>
 <div style=" background: gray;">
 <br>
-	<p style="text-align:center;font-size:14px;height:20px;" > ©️ 2020 Created by Ruba Almuziraee & Ameera Alorf & Reema Alsmaeil</p>
+	<p style="text-align:center;font-size:14px;height:15px;" > ©️ 2020 Created by Ruba Almuziraee & Ameera Alorf & Reema Alsmaeil</p>
 	<br>
 	</div>
 </body>
 </html>
+
